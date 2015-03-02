4e180be95df98ad8d549b8a28c96e9be8249ae52
Li Mingyao <limingyao668@vip.qq.com>
2015-03-02 13:16:57 +0800

    100 104 136

---
+++ 100.cpp
@@ -0,0 +1,23 @@
+// Same Tree
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if(p==NULL&&q==NULL){
+            return true;
+        }
+        if(p!=NULL&&q!=NULL){
+            return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right)&&p->val==q->val;
+        }
+        return false;
+    }
+};
---
+++ 136.cpp
@@ -0,0 +1,12 @@
+// Single Number
+
+class Solution {
+public:
+    int singleNumber(int A[], int n) {
+        int result = 0;
+        for(int i=0;i<n;i++){
+            result^=A[i];
+        }
+        return result;
+    }
+};
