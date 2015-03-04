24a28defa636a7c04008661514bc548891538c77
Li Mingyao <limingyao668@vip.qq.com>
2015-03-04 22:04:24 +0800

    二叉树后序遍历

---
+++ 145.cpp
@@ -0,0 +1,38 @@
+// Binary Tree Postorder Traversal
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
+        vector<int> ans;
+        stack<TreeNode *> s;
+        TreeNode *pre = NULL;
+        if(root!=NULL){
+            s.push(root);
+        }
+        while(!s.empty()){
+            TreeNode *cur = s.top();
+            if((cur->left==NULL&&cur->right==NULL)||(pre!=NULL&&(pre==cur->left||pre==cur->right))){
+                ans.push_back(cur->val);
+                s.pop();
+                pre = cur;
+            }else{
+                if(cur->right!=NULL){
+                    s.push(cur->right);
+                }
+                if(cur->left!=NULL){
+                    s.push(cur->left);
+                }
+            }
+        }
+        return ans;
+    }
+};
