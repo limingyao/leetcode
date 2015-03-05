9d32c86a4322dadf50c63e6d003e2f49d402995c
Li Mingyao <limingyao668@vip.qq.com>
2015-03-05 10:23:52 +0800

    判断二叉平衡树，不使用额外的变量

--- 110.cpp
+++ 110.cpp
@@ -11,7 +11,8 @@
  */
 class Solution {
 public:
-    bool isBalanced(TreeNode *root) {
+    // Method 1:
+    /*bool isBalanced(TreeNode *root) {
         int depth = 0;
         return isBalanced(root, &depth); 
     }
@@ -30,5 +31,20 @@ public:
             
         }
         return false;
+    }*/
+    // Method 2:
+    int dfsHeight (TreeNode *root) {
+        if (root == NULL) return 0;
+
+        int leftHeight = dfsHeight (root -> left);
+        if (leftHeight == -1) return -1;
+        int rightHeight = dfsHeight (root -> right);
+        if (rightHeight == -1) return -1;
+
+        if (abs(leftHeight - rightHeight) > 1)  return -1;
+        return max (leftHeight, rightHeight) + 1;
+    }
+    bool isBalanced(TreeNode *root) {
+        return dfsHeight (root) != -1;
     }
 };
