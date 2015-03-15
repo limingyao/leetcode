7eb6bd4cecc6af2816653c6c5605e49db41913e1
Li Mingyao <limingyao668@vip.qq.com>
2015-03-15 21:09:50 +0800

    二进制中1的个数

---
+++ 191.cpp
@@ -0,0 +1,14 @@
+// Number of 1 Bits
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while(n){
+            ++count;
+            n = n&(n-1);
+        }
+        return count;
+    }
+};
+
