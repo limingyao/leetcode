0398584ee0d80efb9a7446f8385f7e12466f86fd
李明耀 <limingyao@limingyaodeMacBook-Pro.local>
2015-10-01 00:39:45 +0800

    Ugly Number

--- 263.cpp
+++ 263.cpp
@@ -1,3 +1,21 @@
 // Ugly Number
 
-
+class Solution {
+public:
+    bool isUgly(int num) {
+        if(num<1){
+            return false;
+        }
+        int x = num;
+        while(x%2==0){
+            x/=2;
+        }
+        while(x%3==0){
+            x/=3;
+        }
+        while(x%5==0){
+            x/=5;
+        }
+        return x==1?true:false;
+    }
+};
