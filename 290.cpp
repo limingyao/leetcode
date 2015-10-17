ec36a95816721afe0c2db4105c3863c50a64665f
Li Mingyao <limingyao668@vip.qq.com>
2015-10-17 15:09:10 +0800

    Word Pattern

--- 290.cpp
+++ 290.cpp
@@ -17,7 +17,7 @@ public:
                 }
             } else {
                 k_v[pattern[i]] = vec[i];
-                
+
                 if(v_k.find(vec[i])==v_k.end()) {
                     v_k[vec[i]] = pattern[i];
                 } else {
@@ -48,3 +48,24 @@ private:
         return vec;
     }
 };
+
+
+
+bool wordPattern(string pattern, string str) {
+    istringstream strcin(str);
+    string s;
+    vector<string> vs;
+    while(strcin >> s) vs.push_back(s);
+    if (pattern.size() != vs.size()) return false;
+    map<string, char> s2c;
+    map<char, string> c2s;
+    for (int i = 0; i < vs.size(); ++i) {
+        if (s2c[vs[i]] == 0 && c2s[pattern[i]] == "") { 
+            s2c[vs[i]] = pattern[i]; 
+            c2s[pattern[i]] = vs[i]; 
+            continue; 
+        }
+        if (s2c[vs[i]] != pattern[i]) return false;
+    }
+    return true;
+}
