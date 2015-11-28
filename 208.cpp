512af23888184bae13a54e7f1ff7221d16964085
Li Mingyao <limingyao668@vip.qq.com>
2015-11-28 22:35:53 +0800

    update code: Implement Trie (Prefix Tree)

--- 208.cpp
+++ 208.cpp
@@ -3,11 +3,14 @@
 class TrieNode {
 public:
     // Initialize your data structure here.
-    TrieNode() {
-        
-    }
+    TrieNode() { }
     TrieNode* next[26] = {NULL};
     bool flags[26] = {false};
+    ~TrieNode() {
+        for(auto n: next) {
+            delete n;
+        }
+    }
 };
 
 class Trie {
@@ -21,12 +24,10 @@ public:
         size_t len = word.size();
         TrieNode* p = root;
         for(int i=0; i< len; i++) {
-            if(p -> next[word.at(i) - 'a'] == NULL) {
-                p -> next[word.at(i) - 'a'] = new TrieNode();
-                p = p -> next[word.at(i) - 'a'];
-            } else {
-                p = p -> next[word.at(i) - 'a'];
+            if(p -> next[word[i] - 'a'] == NULL) {
+                p -> next[word[i] - 'a'] = new TrieNode();
             }
+            p = p -> next[word[i] - 'a'];
         }
         p -> flags[word.at(len-1) - 'a'] = true;
     }
@@ -34,35 +35,32 @@ public:
     // Returns if the word is in the trie.
     bool search(string word) {
         size_t len = word.size();
-        TrieNode* p = root;
-        for(int i=0; i< len; i++) {
-            if(p -> next[word.at(i) - 'a'] != NULL) {
-                p = p -> next[word.at(i) - 'a'];
-            } else {
-                return false;
-            }
-        }
-        if(!p -> flags[word.at(len-1) - 'a']) return false;
+        TrieNode* p = find(root, word);
+        if(p == NULL || !p -> flags[word[len-1] - 'a']) return false;
         return true;
     }
 
     // Returns if there is any word in the trie
     // that starts with the given prefix.
     bool startsWith(string prefix) {
-        size_t len = prefix.size();
-        TrieNode* p = root;
-        for(int i=0; i< len; i++) {
-            if(p -> next[prefix.at(i) - 'a'] != NULL) {
-                p = p -> next[prefix.at(i) - 'a'];
-            } else {
-                return false;
-            }
-        }
+        TrieNode* p = find(root, prefix);
+        if(p == NULL) return false;
         return true;
     }
 
 private:
     TrieNode* root;
+    TrieNode* find(TrieNode* root, string word) {
+        size_t len = word.size();
+        for(int i=0; i< len; i++) {
+            if(root -> next[word[i] - 'a'] != NULL) {
+                root = root -> next[word[i] - 'a'];
+            } else {
+                return NULL;
+            }
+        }
+        return root;
+    }
 };
 
 // Your Trie object will be instantiated and called as such:
