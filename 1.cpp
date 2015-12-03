cf4ee15f1ba6755fd78398a31cbe41443619a14d
Li Mingyao <limingyao668@vip.qq.com>
2015-12-03 22:55:10 +0800

    remove whitespace

--- 1.cpp
+++ 1.cpp
@@ -8,15 +8,15 @@ public:
         vector<int> result;
         for (int i = 0; i < nums.size(); i++) {
             int numberToFind = target - nums[i];
-    
+
             //if numberToFind is found in map, return them
             if (hash.find(numberToFind) != hash.end()) {
                 //+1 because indices are NOT zero based
                 result.push_back(hash[numberToFind] + 1);
-                result.push_back(i + 1);            
+                result.push_back(i + 1);
                 return result;
             }
-    
+
             //number was not found. Put it in the map.
             hash[nums[i]] = i;
         }
